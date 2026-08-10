3260124ac14694abb69b98b69d1843900d66995c
pancreations <122239080+pancreations@users.noreply.github.com>
2026-08-10 07:02:07 -0500

    chore(C-H4-34): name the rigid floating-hands package

--- tools/package-candidate.ps1
+++ tools/package-candidate.ps1
@@ -122,7 +122,7 @@ try {
 
     $createdUtc = [DateTime]::UtcNow
     $packageId = '{0}-{1}-{2}' -f $commit.Substring(0, 7),
-        'halo4-c33-shared-hand-code',
+        'halo4-c34-rigid-floating-hands',
         $createdUtc.ToString("yyyyMMdd-HHmmssfff'Z'")
     $packageDir = Join-Path $candidateRoot $packageId
     if (Test-Path -LiteralPath $packageDir) {
